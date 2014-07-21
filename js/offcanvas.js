5d0b19e86b231c4855b3b86fbbdcc3064f293eed
Mark Lintern <markllintern@gmail.com>
2014-07-21 11:51:43 -0400

    Function Updates
    Mobile Version
    Always Square Map
    Disabled Scroll Wheel

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -1,6 +1,39 @@
+/*
+ * jQuery Make Square plugin 1.0.0
+ *
+ * Mark Lintern
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ *
+ * Date: July 21 2014
+ */
+jQuery.fn.makeSquare = function(settings) {
+
+    var options = jQuery.extend({
+        based_on: 'width'
+    }, settings);
+
+    $(this).each(function() {
+    	if (options.based_on == 'height') {
+	        var height = $(this).height();
+	        $(this).width(height);
+	    } else {
+	        var width = $(this).width();
+	        $(this).height(width);
+	    }
+    })
+
+};
+
 $(document).ready(function () {
   $('[data-toggle="offcanvas"]').click(function () {
     $('.row-offcanvas').toggleClass('active')
   });
   $('sup').tooltip();
+  $(window).resize(function(){
+  	$('#map-canvas').makeSquare();
+  })
+  $('#map-canvas').makeSquare();
 });
